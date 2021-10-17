967801a6d01ae32d8aa2007440f32c1fae3dadc3
Hassan-Tanoli22 <hassantanoli309@gmail.com>
2021-10-17 14:21:26 +0500

    Second Commit

---
+++ backend/models/product.js
@@ -0,0 +1,110 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required: [true, 'Please enter Product Name'],
+    trim: true,
+    maxLength: [200, 'product name cannot exceed 200 characters']
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'Please enter Product Price'],
+    maxLength: [5, 'product price cannot exceed 5 characters'],
+    default: 0.0
+  },
+
+  description: {
+    type: String,
+    required: [true, 'Please enter Product Description']
+  },
+
+  ratings: {
+    type: number,
+    default: 0
+  },
+
+  images: [
+  {
+    public_id: {
+      type: String,
+      required: true
+    },
+
+    url: {
+      type: String,
+      required: true
+    },
+
+  }
+],
+
+  category: {
+    type: String,
+    required: [true, 'Please enter category for this product'],
+    enum: {
+      values: [
+        'Electronics',
+        'Laptop',
+        'Mobile',
+        'Computers',
+        'Accessories',
+        'MotherBoard',
+        'Processor',
+        'Graphic Card',
+        'Ram',
+        'Hard Disk',
+        'SSD',
+        'KeyBoards',
+        'Mouse',
+        'Headphones'
+      ],
+      message: 'Please select the following catogorize'
+    }
+  },
+
+    seller: {
+      type: String,
+      required: [true, 'Please enter product seller']
+    },
+
+    stock: {
+        type: Number,
+        required: [true, 'Please enter product stock'],
+        maxLength: [50, 'product cannot exceed 5 characters'],
+        default: 0
+    },
+
+    numOfReviews: {
+      type: Number,
+      default: 0
+    },
+
+    reviews: [
+      {
+        name: {
+          type: String,
+          required: true
+        },
+
+        rating: {
+          type: Number,
+          required: true
+        },
+        comment: {
+          type: String,
+          required: true
+        }
+      }
+    ],
+
+    createdAt: {
+      type: Date,
+      default: Date.now 
+    }
+
+})
+
+module.exports = mongoose.model('product', productSchema);
