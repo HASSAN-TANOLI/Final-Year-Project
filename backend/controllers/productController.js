c923df74a7f91f5f9d53bb3d65c1aa3da5429a45
Hassan-Tanoli22 <hassantanoli309@gmail.com>
2021-10-21 00:44:20 +0500

    Done with Search and Filter

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,14 +19,19 @@ exports.newProduct = catchAsyncErrors (async (req,res, next) => {
 // Get all products => /api/v1/products
 exports.getproducts =catchAsyncErrors (async (req, res, next) => {
 
+  const resPerPage = 4;
+  const productCount = await Product.countDocuments();
   const apiFeatures = new APIFeatures(Product.find(), req.query)
                                       .search()
+                                      .filter()
+                                      .pagination(resPerPage)
 
   const products = await apiFeatures.query;
   res.status(200).json({
 
     success: true,
     count: products.length,
+    productCount,
     products
   })
 })
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -7,23 +7,53 @@ class APIFeatures {
   }
 
   //Search
-  Search()
+  search()
   {
-  const Keyword = this.queryStr.Keyword ? {
+  const keyword = this.queryStr.keyword ? {
     name: {
       $regex: this.queryStr.keyword,
       $options: 'i'
-
-    }
+    } 
 
 
   } : {}
 
-  console.log(Keyword);
+  console.log(keyword);
 
-  this.query = this.query.find ( {...Keyword });
+  this.query = this.query.find ( {...keyword });
   return this;
 }
+
+    filter()
+    {
+       const queryCopy = {...this.queryStr};
+
+       
+       //Removing fields from the query
+
+       const removeFields = ['keyword', 'limit', 'page']
+       removeFields.forEach(el => delete queryCopy[el]);
+      
+       
+
+       //Advance filtering for price and rating 
+       let queryStr = JSON.stringify(queryCopy)
+      queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+      
+
+       this.query = this.query.find(JSON.parse(queryStr));
+       return this;
+    }
+
+    pagination(resPerPage)
+    {
+      const currentPage = Number(this.queryStr.page) || 1;
+      const skip = resPerPage * (currentPage - 1);
+
+      this.query = this.query.limit(resPerPage).skip(skip);
+      return this;
+    }
 }
 
 module.exports = APIFeatures
